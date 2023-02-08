242d09c5c84e6d33837fab981188fc804fda69f6
DESKTOP-LNFCMVA\HP <sixaruliketi@gmail.com>
2023-02-08 16:32:17 +0400

    appointment fragment (easy ver.) added

--- app/src/main/java/com/example/medicalbloggerapp/appointment/AppointmentFragment.kt
+++ app/src/main/java/com/example/medicalbloggerapp/appointment/AppointmentFragment.kt
@@ -1,7 +1,43 @@
 package com.example.medicalbloggerapp.appointment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.medicalbloggerapp.R
+import com.example.medicalbloggerapp.SampleData
+import com.example.medicalbloggerapp.databinding.FragmentAppointmentBinding
+import com.example.medicalbloggerapp.databinding.FragmentHomeBinding
+import com.example.medicalbloggerapp.home.homeAdapters.HomeMainAdapter
 
-class AppointmentFragment : Fragment(R.layout.fragment_appointment) {
+class AppointmentFragment : Fragment() {
+
+    private var _binding: FragmentAppointmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAppointmentBinding.inflate(inflater, container, false)
+        init()
+        return binding.root
+    }
+
+    private fun init(){
+        binding.apply {
+            addAppointmentClickButton.setOnClickListener{
+                Toast.makeText(context, "you're in the queue", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
